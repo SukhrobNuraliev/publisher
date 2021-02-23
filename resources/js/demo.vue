16fa03f141e16ea118762323f66bfca31460e0fa
Sukhrob Nuraliev <=>
2021-02-23 21:30:56 +0500

    inertia installed & markup pages created

---
+++ resources/js/demo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
+                Dashboard
+            </inertia-link>
+
+            <template v-else>
+                <inertia-link :href="route('login')" class="text-sm text-gray-700 underline">
+                    Log in
+                </inertia-link>
+
+                <inertia-link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
+                    Register
+                </inertia-link>
+            </template>
+        </div>
+</template>
